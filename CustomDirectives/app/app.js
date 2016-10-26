bb816b29055a0f49bbf41cc0fb93d5deb4e65571
Vaibhav Bali <vibsbali@gmail.com>
2016-10-26 19:08:59 +1100

    Created property expression to be used with scope.$eval

---
+++ CustomDirectives/app/app.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+(function() {
+    angular.module("exampleApp", [])
+        .controller("defaultController", function () {
+
+            var vm = this;
+            vm.products = [
+                { name: "Apples", category: "Fruit", price: 1.20, expiry: 10 },
+                { name: "Bananas", category: "Fruit", price: 2.42, expiry: 7 },
+                { name: "Pears", category: "Fruit", price: 2.02, expiry: 6 }
+            ];
+        });
+}());
