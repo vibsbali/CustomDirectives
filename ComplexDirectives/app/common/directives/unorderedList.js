29df34bb0e4ce5b9a0177fcea1e41c90e639f8a2
Vaibhav Bali <vibsbali@gmail.com>
2016-10-27 19:02:28 +1100

    Returning templateUrl as a function

--- ComplexDirectives/app/common/directives/unorderedList.js
+++ ComplexDirectives/app/common/directives/unorderedList.js
@@ -2,25 +2,24 @@
 (function () {
     angular.module("exampleApp")
         .directive("unorderedList", function () {
-            return function (scope, element, attrs) {
-
-                var dataValue = attrs["propertyName"];
-                var data = scope.$eval(dataValue);
-
-                //var data = scope.vm.products;
-                var propertyExpression = attrs["listProperty"];
-
-
-                if (angular.isArray(data)) {
-                    var listElem = angular.element("<ul>");
-                    element.append(listElem);
-                    for (var i = 0; i < data.length; i++) {
-                        var itemElement = angular.element("<li>").text(scope.$eval(propertyExpression, data[i]));
-                        listElem.append(itemElement);
-                    }
-
-                    //listElem.append(angular.element("<li>").text(scope.$eval(propertyExpression, data[i])));
+            return{
+                restrict: "AE",
+                templateUrl: function () { return "/app/common/templates/itemTemplate.html" },
+                link: function (scope, element, attrs) {
+                    var dataValue = attrs["propertyName"] || attrs["unorderedList"];
+                    scope.data = scope.$eval(dataValue);
+                    //var data = scope.vm.products;
+                    var propertyExpression = attrs["listProperty"] || "price | currency";
+                    
+                    //if (angular.isArray(data)) {
+                    //    var listElem = angular.element("<ul>");
+                    //    element.append(listElem);
+                    //    for (var i = 0; i < data.length; i++) {
+                    //        var itemElement = angular.element("<li>").text(scope.$eval(propertyExpression, data[i]));
+                    //        listElem.append(itemElement);
+                    //    }
+                    //}
                 }
-            }
+            };
         });
 }());
