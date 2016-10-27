9733362040cb68acf2499e514caba5d982e9e51a
Vaibhav Bali <vibsbali@gmail.com>
2016-10-27 18:25:23 +1100

    Created a new appliction for complex directives

---
+++ ComplexDirectives/app/app.js
@@ -0,0 +1,20 @@
+﻿"use strict";
+
+(function() {
+    angular.module("exampleApp", [])
+        .controller("defaultController", function () {
+
+            var vm = this;
+            vm.products = [
+                { name: "Apples", category: "Fruit", price: 1.20, expiry: 10 },
+                { name: "Bananas", category: "Fruit", price: 2.42, expiry: 7 },
+                { name: "Pears", category: "Fruit", price: 2.02, expiry: 6 }
+            ];
+
+            vm.incrementPrices = function() {
+                for (var i = 0; i < vm.products.length; i++) {
+                    vm.products[i].price++;
+                }
+            };
+        });
+}());
---
+++ ComplexDirectives/app/common/directives/unorderedList.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+(function () {
+    angular.module("exampleApp")
+        .directive("unorderedList", function () {
+            return function (scope, element, attrs) {
+
+                var dataValue = attrs["propertyName"];
+                var data = scope.$eval(dataValue);
+
+                //var data = scope.vm.products;
+                var propertyExpression = attrs["listProperty"];
+
+
+                if (angular.isArray(data)) {
+                    var listElem = angular.element("<ul>");
+                    element.append(listElem);
+                    for (var i = 0; i < data.length; i++) {
+                        var itemElement = angular.element("<li>").text(scope.$eval(propertyExpression, data[i]));
+                        listElem.append(itemElement);
+                    }
+
+                    //listElem.append(angular.element("<li>").text(scope.$eval(propertyExpression, data[i])));
+                }
+            }
+        });
+}());
